1e626e5e2c4b8f439e6bf44bc9afdba90760c7a7
Erik <erik.r.svensson@telia.com>
2020-10-01 14:52:51 +0200

    U-Net with ReLU

--- ann.py
+++ ann.py
@@ -46,51 +46,76 @@ class U_Net(nn.Module):
 		super(U_Net, self).__init__()
 		self.encoder1 = nn.Sequential(
 			nn.Conv2d(1, 32, kernel_size=3, padding=1),
+			nn.ReLU(),
 			#nn.BatchNorm2d(32),
 			nn.Conv2d(32, 32, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(32, 32, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(32, 32, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(32, 32, kernel_size=3, padding=1),
+			nn.ReLU(),
 		)
 
 		self.encoder2 = nn.Sequential(
 			nn.MaxPool2d(kernel_size=2, stride=2),
 			nn.Conv2d(32, 64, kernel_size=3, padding=1),
+			nn.ReLU(),
 			#nn.BatchNorm2d(64),
 			nn.Conv2d(64, 64, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(64, 64, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(64, 64, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(64, 64, kernel_size=3, padding=1),
+			nn.ReLU(),
 		)
 
 		self.encoder3 = nn.Sequential(
 			nn.MaxPool2d(kernel_size=2, stride=2),
 			nn.Conv2d(64, 128, kernel_size=3, padding=1),
+			nn.ReLU(),
 			#nn.BatchNorm2d(128),
 			nn.Conv2d(128, 128, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(128, 128, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(128, 128, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(128, 128, kernel_size=3, padding=1),
+			nn.ReLU(),
 		)
 
 		self.encoder4 = nn.Sequential(
 			nn.MaxPool2d(kernel_size=2, stride=2),
 			nn.Conv2d(128, 256, kernel_size=3, padding=1),
+			nn.ReLU(),
 			#nn.BatchNorm2d(256),
 			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.ReLU(),
 		)
 
 		self.encoder5 = nn.Sequential(
 			nn.MaxPool2d(kernel_size=2, stride=2),
 			nn.Conv2d(256, 512, kernel_size=3, padding=1),
+			nn.ReLU(),
 			#nn.BatchNorm2d(512),
 			nn.Conv2d(512, 512, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(512, 512, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(512, 512, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(512, 512, kernel_size=3, padding=1),
+			nn.ReLU(),
 		)
 
 		self.bottleneck= nn.Sequential(
@@ -105,50 +130,74 @@ class U_Net(nn.Module):
 		
 		self.decoder1 = nn.Sequential(
 			nn.Conv2d(1024, 512, kernel_size=3, padding=1),
+			nn.ReLU(),
 			#nn.BatchNorm2d(512),
 			nn.Conv2d(512, 512, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(512, 512, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(512, 512, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(512, 512, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2),
 		)
 
 		self.decoder2 = nn.Sequential(
 			nn.Conv2d(512, 256, kernel_size=3, padding=1),
+			nn.ReLU(),
 			#nn.BatchNorm2d(256),
 			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
 		)
 
 		self.decoder3 = nn.Sequential(
 			nn.Conv2d(256, 128, kernel_size=3, padding=1),
+			nn.ReLU(),
 			#nn.BatchNorm2d(128),
 			nn.Conv2d(128, 128, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(128, 128, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(128, 128, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(128, 128, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
 		)
 
 		self.decoder4 = nn.Sequential(
 			nn.Conv2d(128, 64, kernel_size=3, padding=1),
+			nn.ReLU(),
 			#nn.BatchNorm2d(64),
 			nn.Conv2d(64, 64, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(64, 64, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(64, 64, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(64, 64, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
 		)
 
 		self.decoder5 = nn.Sequential(
 			nn.Conv2d(64, 32, kernel_size=3, padding=1),
+			nn.ReLU(),
 			#nn.BatchNorm2d(32),
 			nn.Conv2d(32, 32, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(32, 32, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(32, 32, kernel_size=3, padding=1),
+			nn.ReLU(),
 			nn.Conv2d(32, 3, kernel_size=3, padding=1),
 			nn.Sigmoid()
 		)
