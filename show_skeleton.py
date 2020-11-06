57409539fc1e64703d317ae3cba672ab8749a5e0
Erik <erik.r.svensson@telia.com>
2020-11-06 19:24:09 +0100

    Show image skeleton

---
+++ show_skeleton.py
@@ -0,0 +1,34 @@
+import torch 
+import torchvision.transforms as transforms
+
+from PIL import Image
+
+from ann import *
+
+
+image_to_tensor = transforms.ToTensor()
+tensor_to_image = transforms.ToPILImage()
+
+
+# Takes a pil image and a network to show the image's skeleton
+def show_image_skeleton(img, net):
+	data = torch.unsqueeze( image_to_tensor(img), 0 ).cuda()
+
+	output = torch.squeeze( net(data).detach().cpu() )
+
+	output_image = tensor_to_image( output )
+
+
+	output_image.show()
+
+
+
+
+#img_bat = Image.open("Data/img_train_shape/bat-1.png")
+
+#ann = torch.load("skeleton_net.pt")
+
+
+#show_image_skeleton(img_bat, ann)
+
+
